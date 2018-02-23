a609c1404dfc08488795e52e17acb09a86293fdb
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2018-02-23 22:11:42 +0100

    Expand package description

--- mix.exs
+++ mix.exs
@@ -7,11 +7,15 @@ defmodule Exyt.Mixfile do
   def project do
     [
       app: :exyt,
-      name: "Exyt Youtube API client",
+      name: "Exyt",
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
+      description: description(),
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -22,10 +26,33 @@ defmodule Exyt.Mixfile do
     ]
   end
 
+  defp description do
+    "An Elixir client library for the Youtube API v3"
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme"
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Sebastian Ziebell"],
+      licenses: ["MIT"],
+      links: %{github: @project_url}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
